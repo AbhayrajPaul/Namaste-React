7c947706ea650562e1807f7524613435a917713e
AbhayrajPaul <2k15raj@gmail.com>
2023-10-22 08:00:52 +0530

    01

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello World!"); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); // converting into a tag and putting it up
