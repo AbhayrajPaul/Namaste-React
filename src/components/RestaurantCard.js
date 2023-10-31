3405f69b18dcbef25ef3d6e0dcafc29e6e99476f
AbhayrajPaul <123346604+AbhayrajPaul@users.noreply.github.com>
2023-10-31 21:12:09 +0530

    Add files via upload

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,27 +1,22 @@
 import { CDN_URL } from "../utils/constants";
 
-const RestaurantCard = (props) => {
-  const { restaurantData } = props;
-
-  const {
-    cloudinaryImageId,
-    name,
-    cuisines,
-    avgRating,
-    costForTwo,
-    deliveryTime,
-  } = restaurantData?.data;
-
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  cuisines,
+  avgRating,
+  costForTwo,
+  deliveryTime,
+}) => {
   return (
     <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img className="res-logo" src={CDN_URL + cloudinaryImageId} alt="logo" />
       <h3>{name}</h3>
       <h4>{cuisines.join(",")}</h4>
       <h4>{avgRating} Stars</h4>
-      <h4> ₹{costForTwo / 100}/-</h4>
+      <h4> Rs {costForTwo}/-</h4>
       <h4>{deliveryTime} mins</h4>
     </div>
   ); // converting the costForTwo in inr
 };
-
 export default RestaurantCard;
